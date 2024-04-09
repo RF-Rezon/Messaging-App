08d80240c0eafd367d04202029d83c530e26f991
Rezwan Faysal <rezwanfaysalrezon@gmail.com>
2024-04-09 20:56:45 +0600

    firebase set. Prisma start

---
+++ client/src/utils/FirebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA8Bw7guoxdNEeV1IYAfQY7GfGvGMmd_Xo",
+    authDomain: "message-app-d66d2.firebaseapp.com",
+    projectId: "message-app-d66d2",
+    storageBucket: "message-app-d66d2.appspot.com",
+    messagingSenderId: "1041808880000",
+    appId: "1:1041808880000:web:0f57ecb9ef4264042e5165",
+    measurementId: "G-6R8813P1NT"
+};
+
+
+const app = initializeApp(firebaseConfig);
